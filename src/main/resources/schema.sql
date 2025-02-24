1ba3c5ddb76ee3ab7c369dccf8eb9e6670f311eb
Pavel Ryabykh <pryabykh@gmail.com>
2025-02-24 10:24:41 +0300

    Добавить if not exists для создания таблиц и индексов

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create schema if not exists myblog;
 
-create table myblog.posts
+create table if not exists myblog.posts
 (
     id            serial primary key,
     title         text   not null,
@@ -9,26 +9,26 @@ create table myblog.posts
     likes         bigint not null default 0
 );
 
-create table myblog.comments
+create table if not exists myblog.comments
 (
     id      serial primary key,
     content text   not null,
     post_id bigint not null references myblog.posts (id)
 );
 
-create index myblog_comments_post_id_index on myblog.comments (post_id);
+create index if not exists myblog_comments_post_id_index on myblog.comments (post_id);
 
-create table myblog.tags
+create table if not exists myblog.tags
 (
     id      serial primary key,
     content text not null
 );
 
-create table myblog.posts_tags
+create table if not exists myblog.posts_tags
 (
     post_id bigint not null references myblog.posts (id),
     tag_id  bigint not null references myblog.tags (id)
 );
 
-create index myblog_posts_tags_post_id_index on myblog.posts_tags (post_id);
-create index myblog_posts_tags_tag_id_index on myblog.posts_tags (tag_id);
+create index if not exists myblog_posts_tags_post_id_index on myblog.posts_tags (post_id);
+create index if not exists myblog_posts_tags_tag_id_index on myblog.posts_tags (tag_id);
