ea4d463f077f10191beb553e88a2a8e61ed2f40a
Pavel Ryabykh <pryabykh@gmail.com>
2025-02-26 14:29:01 +0300

    UI: отображение поста

--- src/main/java/com/pryabykh/controller/BlogController.java
+++ src/main/java/com/pryabykh/controller/BlogController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,5 +49,12 @@ public class BlogController {
     @GetMapping("/create-post-form")
     public String createPost() {
         return "create-post";
+    }
+
+    @GetMapping("/post/{id}")
+    public String createPost(@PathVariable("id") Long id, Model model) {
+        PostDto post = blogService.findPostById(id);
+        model.addAttribute("post", post);
+        return "post";
     }
 }
