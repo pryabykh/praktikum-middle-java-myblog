2d4f34aaf3933611ab3287e582ec3e69ef0a4dd0
Pavel Ryabykh <pryabykh@gmail.com>
2025-03-11 19:13:27 +0300

    Инициализация проекта Gradle

---
+++ src/test/java/com/pryabykh/myblog/MyblogApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pryabykh.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
